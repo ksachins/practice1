0e42eaceda49fdfdffc3347107e103a27f1e2bdb
Kammu <gauravkumar@Kamakshis-MacBook-Air.local>
2022-08-10 13:03:13 +0530

    commit

---
+++ src/main/java/org/cap/demo/Practice1Application.java
@@ -0,0 +1,16 @@
+package org.cap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class Practice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice1Application.class, args);
+	}
+
+}
